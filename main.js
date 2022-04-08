ec1a1f91a3dbb695b6c596e1d3c78b5d994bd5b7
Maanu42 <76243940+Maanu42@users.noreply.github.com>
2022-04-08 21:25:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+song1="";
+song2="";
+function setup(){
+    canvas= createCanvas(600,500);
+    canvas.center();
+
+    video= createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video,0,0,600,500);
+}
